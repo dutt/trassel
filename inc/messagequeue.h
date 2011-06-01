7cdf7094f7328238cebbe1b68d118099ef240349
Mikael Modin <micke.modin@gmail.com>
2011-06-02 00:37:34 +0200

    Added wait/reply-support for all message types.

--- inc/messagequeue.h
+++ inc/messagequeue.h
@@ -127,16 +127,19 @@ public:
 class MessageClient : public DirectedConsumer, public DirectedProducer {
 	static uint8 lastID;
 	uint8 mID;
+
+	Message* createMessage(MessageClient* receiver, MsgType::MsgTypeEnum type);
+	Message* waitAsync(Message* msg, bool waitForReply);
 public:
 	MessageClient() : mID(lastID++) {}
 
 	uint8 getID() { return mID; }
-
+	
 	Message* receiveMessage();
 	Message* sendMessage(BoolMsg& data,MessageClient* receiver, bool async = true, bool waitForReply = false);
 	Message* sendReply(Message* previous, BoolMsg& data, bool async = true, bool waitForReply = false);
-	void sendMessage(StringMsg& data, MessageClient* receiver, bool async = true);
-	void sendMessage(DataMsg& data, MessageClient* receiver, bool async = true);
+	Message* sendMessage(StringMsg& data, MessageClient* receiver, bool async = true, bool waitForReply = false);
+	Message* sendMessage(DataMsg& data, MessageClient* receiver, bool async = true, bool waitForReply = false);
 	
 };
 
--- src/messagequeue.cpp
+++ src/messagequeue.cpp
@@ -15,61 +15,62 @@ Message* MessageClient::receiveMessage() {
 	return consume(mID);
 }
 
-Message* MessageClient::sendMessage(BoolMsg& data, MessageClient* receiver, bool async, bool waitForReply) {
+Message* MessageClient::createMessage(MessageClient* receiver, MsgType::MsgTypeEnum type) {
 	Message* msg = new Message();
 	msg->previous = msg->next = 0;
 	msg->sender = this;
 	msg->receiver = receiver;
-	msg->type = MsgType::BoolMsgType;
+	msg->type = type;
+	return msg;
+}
+Message* MessageClient::waitAsync(Message* msg, bool waitForReply) {
+	boost::unique_lock<boost::mutex> lock(msg->mMutex);
+	msg->waitCondition.wait(lock);
+	if(waitForReply) {
+		Message* reply = receiveMessage();
+		return reply;
+	}
+	else
+		return 0;
+}
+Message* MessageClient::sendMessage(BoolMsg& data, MessageClient* receiver, bool async, bool waitForReply) {
+	Message* msg = createMessage(receiver, MsgType::BoolMsgType);
 	msg->boolMsg = data;
 	produce(msg);
 	if(!async) {
-		boost::unique_lock<boost::mutex> lock(msg->mMutex);
-		msg->waitCondition.wait(lock);
-		if(waitForReply) {
-			Message* reply = receiveMessage();
-			return reply;
-		}
+		return waitAsync(msg, waitForReply);
 	}
 	return 0;
 }
 
 Message* MessageClient::sendReply(Message* previous, BoolMsg& data, bool async, bool waitForReply) {
-	Message* msg = new Message();
+	Message* msg = createMessage(previous->sender, MsgType::BoolMsgType);
 	previous->next = msg;
 	msg->previous = previous;
-	msg->next = 0;
-	msg->sender = this;
-	msg->receiver = previous->sender;
 	msg->boolMsg = data;
-	msg->type = MsgType::BoolMsgType;
 	produce(msg);
 	if(!async) {
-		boost::unique_lock<boost::mutex> lock(msg->mMutex);
-		msg->waitCondition.wait(lock);
-		if(waitForReply) {
-			Message* reply = receiveMessage();
-			reply->previous = msg;
-			return reply;
-		}
+		return waitAsync(msg, waitForReply);
 	}
 	return 0;
 }
 
-void MessageClient::sendMessage(StringMsg& data, MessageClient* receiver, bool async) {
-	Message* msg = new Message();
-	msg->sender = this;
-	msg->receiver = receiver;
-	msg->type = MsgType::StringMsgType;
+Message* MessageClient::sendMessage(StringMsg& data, MessageClient* receiver, bool async, bool waitForReply) {
+	Message* msg = createMessage(receiver, MsgType::StringMsgType);
 	msg->stringMsg = data;
 	produce(msg);
+	if(!async) {
+		return waitAsync(msg, waitForReply);
+	}
+	return 0;
 }
 
-void MessageClient::sendMessage(DataMsg& data, MessageClient* receiver, bool async) {
-	Message* msg = new Message();
-	msg->sender = this;
-	msg->receiver = receiver;
-	msg->type = MsgType::DataMsgType;
+Message* MessageClient::sendMessage(DataMsg& data, MessageClient* receiver, bool async, bool waitForReply) {
+	Message* msg = createMessage(receiver, MsgType::DataMsgType);
 	msg->dataMsg = data;
 	produce(msg);
+	if(!async) {
+		return waitAsync(msg, waitForReply);
+	}
+	return 0;
 }
